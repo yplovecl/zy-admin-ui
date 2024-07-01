c3f2fed2bcec9a1aa3282845a87c04a9dd4dfb83
yangpan <yplovecl@sina.com>
2024-07-01 09:32:19 +0800

    系统名称修改

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <el-form ref="loginRef" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">Blazer数字地震仪安卓系统</h3>
+      <h3 class="title">弹着点实时定位系统</h3>
       <el-form-item prop="username">
         <el-input
           v-model="loginForm.username"
