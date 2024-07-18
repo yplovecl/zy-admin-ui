72f8339135a3f439d95d6e0e04354a72570b79f9
yangpan <yplovecl@sina.com>
2024-07-18 13:45:19 +0800

    弹着点实时定位系统

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -25,7 +25,7 @@ defineProps({
   }
 })
 
-const title = ref('Blazer数字地震仪');
+const title = ref('弹着点实时定位系统');
 const settingsStore = useSettingsStore();
 const sideTheme = computed(() => settingsStore.sideTheme);
 </script>
